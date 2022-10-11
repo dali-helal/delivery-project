804100dc29b1332ad1a1967de9d29c25b46d05ef
Dali <dalihelal6@gmail.com>
2022-10-11 18:37:21 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,9 @@ const Home = () => {
    const FetchData = async () => {
       const response = await fetch(" https://food-delivery-react-js.herokuapp.com/api/restaurant")
       const json = await response.json()
+
+      await new Promise(r => setTimeout(r, 750));
+
       setData(json)
       setIspending(false)
    }
@@ -19,30 +22,71 @@ const Home = () => {
 
    return (
       <section className="home">
-         <div className="container-grid">
-            {
-               data && data.map((restaurant, index) => {
-                  return (
-                    <a href={`/restaurant/${restaurant._id}`} >
-                        <div className="conatiner-product">
-                           <img src={restaurant.Img} alt="" />
-                           <p>Delivers in 40 min</p>
-                           <h4>{restaurant.NameRestaurant}</h4>
-                           <div className="ratings">
-                              <FaStar color="#FFA400" />
-                              <FaStar color="#FFA400" />
-                              <FaStar color="#FFA400" />
-                              <FaStar color="#FFA400" />
-                              <p>{restaurant.Ratings} ratings</p>
-                           </ div>
-                        </div>
-                     </a>
+         {
+            isPending ? <div className="containerIsPending">
+               <div className="skeleton">
+                  <div className="img"></div>
+                  <div className="wrap">
+                     <div className="line"></div>
+                     <div className="line"></div>
+                     <div className="line"></div>
+                  </div>
+               </div>
+               <div className="skeleton">
+                  <div className="img"></div>
+                  <div className="wrap">
+                     <div className="line"></div>
+                     <div className="line"></div>
+                     <div className="line"></div>
+                  </div>
+               </div>
+               <div className="skeleton">
+                  <div className="img"></div>
+                  <div className="wrap">
+                     <div className="line"></div>
+                     <div className="line"></div>
+                     <div className="line"></div>
+                  </div>
+               </div>
+               <div className="skeleton">
+                  <div className="img"></div>
+                  <div className="wrap">
+                     <div className="line"></div>
+                     <div className="line"></div>
+                     <div className="line"></div>
+                  </div>
+               </div>
+            </div>
 
-                  )
-               }
+               : <div className="container-grid">
 
-               )}
-         </div>
+                  {
+                     data && data.map((restaurant, index) => {
+                        return (
+                           <a href={`/restaurant/${restaurant._id}`} >
+                              <div className="conatiner-product">
+                                 <img src={restaurant.Img} alt="" />
+                                 <p>Delivers in 40 min</p>
+                                 <h4>{restaurant.NameRestaurant}</h4>
+                                 <div className="ratings">
+                                    <FaStar color="#FFA400" />
+                                    <FaStar color="#FFA400" />
+                                    <FaStar color="#FFA400" />
+                                    <FaStar color="#FFA400" />
+                                    <FaStar color="#FFA400" />
+                                    <p>{restaurant.Ratings} ratings</p>
+                                 </ div>
+                              </div>
+                           </a>
+
+                        )
+                     }
+
+                     )}
+               </div>
+
+         }
+
       </section>
    )
 }
